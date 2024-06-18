ea26a80c4c4c2a5ff3a171316145153cbe4d8df9
Sondinh <dnson20022001@gmail.com>
2024-06-18 18:21:19 +0700

    Add git config && small update to other configs

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PRESENT=false
 
-DOT_FOLDERS="alacritty,bash,powerlevel10k,zsh"
+DOT_FOLDERS="alacritty,bash,powerlevel10k,zsh,wallpapers"
 
 echo "[+] Dotfiles :: $SCRIPT_DIR"
 
