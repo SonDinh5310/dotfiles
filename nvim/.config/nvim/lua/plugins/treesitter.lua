77fd71355ae8716fcb007f779e9c9d740056861d
Sondinh <dnson20022001@gmail.com>
2024-06-25 14:29:05 +0700

    Update configs

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,15 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = { "html", "css", "javascript", "typescript", "lua" },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
+		configs.setup({
+			ensure_installed = { "html", "css", "javascript", "typescript", "tsx", "lua" },
+			sync_install = false,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
